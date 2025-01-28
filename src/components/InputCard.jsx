0c002f0b9f2970bd038b1eb19db748d4d5ef9bb9
esmern2403 <you email>
2025-01-28 21:48:42 +0600

    banner part responsive done

--- src/components/InputCard.jsx
+++ src/components/InputCard.jsx
@@ -6,12 +6,12 @@ import { IoIosArrowDown } from 'react-icons/io';
 const InputCard = () => {
   return (
     <div>
-        <Flex className='gap-x-3 items-center'>
+        <Flex className='md:gap-x-3 items-center'>
         <IoLocationOutline  className='text-secondary'/>
-        <p className='text-base text-four font-bold font-nun'>Locition</p>
+        <p className='text-xs md:text-base text-four font-bold font-nun'>Locition</p>
         <IoIosArrowDown  className='text-secondary'/>
         </Flex>
-        <p className='text-base text-third font-normal font-nun pt-2 pl-7'>Enter your location</p>
+        <p className='text-xs md:text-base text-third font-normal font-nun pt-2 pl-[10px] md:pl-7'>Enter your location</p>
     </div>
   )
 }
--- src/layouts/Banner.jsx
+++ src/layouts/Banner.jsx
@@ -12,23 +12,23 @@ import { IoLocationOutline } from "react-icons/io5";
 const Banner = () => {
   return (
     
-    <section className='bg-[#F8F8F8] py-[135px] relative'>
-      <div className='absolute top-[300px] right-[80px]'><Image className='w-[80%]'  src={BannerOne}/></div>
-      <div className='absolute top-[446px] right-[420px]'><Image className='w-[80%]' src={BannerTwo}/></div>
+    <section className='bg-[#F8F8F8] py-16 md:py-[135px] relative'>
+      <div className='hidden md:block absolute top-[300px] right-[80px]'><Image className='w-[80%]'  src={BannerOne}/></div>
+      <div className='hidden md:block absolute top-[446px] right-[420px]'><Image className='w-[80%]' src={BannerTwo}/></div>
       <Container>
-        <Flex>
-          <div className='w-1/2'>
-           <h1 className='text-[65px] text-primary font-semibold font-pop leading-[100px]'>Start Exploring to Find Inner Peace by Traveling</h1>
-          <p className='w-[597px] text-base text-third font-semibold font-pop pt-8 pb-10'>pLost yourself with Vacation and Travel into the place that you never visit before. We have thousand places in our database for you.</p>
-          <Button className='mb-10' text='Discover Now'/>
+        <Flex className='!block'>
+          <div className='md:w-1/2 px-3'>
+           <h1 className='text-3xl md:text-[65px] text-primary font-semibold font-pop leading-[50px] md:leading-[100px]'>Start Exploring to Find Inner Peace by Traveling</h1>
+          <p className='w-auto md:w-[597px] text-base text-third font-semibold font-pop pt-8 pb-10'>pLost yourself with Vacation and Travel into the place that you never visit before. We have thousand places in our database for you.</p>
+          <Button className='mb-10 py-2.5 px-10' text='Discover Now'/>
 
-          <div className='relative w-[825px] bg-white rounded-[10px]'>
-            <Flex  className='py-9 px-6 gap-x-[70px]'>
+          <div className='relative md:w-[825px] bg-white rounded-[10px]'>
+            <Flex  className='p-[10px] md:py-9 md:px-6 '>
               <InputCard/>
                <InputCard/>
                <InputCard/>
               </Flex>
-              <button className='absolute top-1/2 -translate-y-1/2 right-6 p-[30px] bg-secondary rounded-[10px]'><CiSearch  className='text-white text-xl'/></button>
+              <button className='absolute top-[136%] py-3 px-[138px] -translate-y-1/2 md:right-6 p-[30px] bg-secondary rounded-[10px]'><CiSearch  className='text-white text-xl'/></button>
             
 
           </div>
